4fb9664da3c9af01c2a8240323390f43a4541e39
Francis Vargas <fvargas213@gmail.com>
2016-03-17 17:02:20 -0400

    added Nav func

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,16 @@
-// (function () {
+(function () {
   
-//   var  = document.querySelectorAll('.first-level > li');
+  var burger = document.querySelector('.burger-icon');
+  var mobNav = document.querySelector('nav.mobile');
+    
+  burger.addEventListener('click', function () {
+    if (!this.classList.contains('active')) {
+      this.classList.add('active');
+      mobNav.classList.add('active');
+    } else {
+      this.classList.remove('active');
+      mobNav.classList.remove('active');
+    }
+  });
 
-//   for (var i = 0; i < navElems.length; i++) {
-//     navElem = navElems[i];
-
-//     navElem.addEventListener('mouseover', function () {
-//       if (!this.classList.contains('active')) {
-//         this.classList.add('active');
-//       } 
-//     });
-
-//     navElem.addEventListener('mouseout', function () {
-//       this.classList.remove('active');
-//     });
-//   };
-
-// })();
+})();
