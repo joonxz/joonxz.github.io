7bb2cc25a8588f12ae39cb2515181fe3c4518620
Francis Vargas <fvargas213@gmail.com>
2016-03-17 17:45:57 -0400

    disable mob nav if active

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,9 +2,16 @@
   
   var burger = document.querySelector('.burger-icon');
   var mobNav = document.querySelector('nav.mobile');
-    
+
+  window.addEventListener('resize', function () {
+    if(window.innerWidth > '461') {
+      burger.classList.remove('active');
+      mobNav.classList.remove('active');
+    }
+  });
+
   burger.addEventListener('click', function () {
-    if (!this.classList.contains('active')) {
+    if (!this.classList.contains('active') && window.innerWidth <= '461') {
       this.classList.add('active');
       mobNav.classList.add('active');
     } else {
